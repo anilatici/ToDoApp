c0e21b17d2a1d080b663d02bf00ad40bf8faf99e
anilatici <anillatici@gmail.com>
2024-08-23 19:31:02 +0100

    refactor: Update main.dart to use const widgets and remove unused code

--- to_do_list/lib/main.dart
+++ to_do_list/lib/main.dart
@@ -1,66 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_list/pages/home.dart';
 
 void main() => runApp(MainApp());
-
-// MaterialApp
-// Scaffold
-// Appbar
-// Text
-// body: Column
-// tasks
 
 class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'To Do App',
-      theme: ThemeData(primarySwatch: Colors.grey),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('My To Do List'),
-      ),
-      body: ListView(
-        children: [
-          TaskList(),
-        ],
-      ),
-    );
-  }
-}
-
-class TaskList extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Task(label: "Do grocery shopping"),
-        Task(label: "Clean the house"),
-        Task(label: "Practice the guitar"),
-      ],
-    );
-  }
-}
-
-class Task extends StatelessWidget {
-  final String label;
-
-  const Task({Key? key, required this.label}) : super(key: key); // Updated constructor
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Checkbox(onChanged: null, value: false),
-        Text(label),
-      ],
+      home: HomePage(),
     );
   }
 }
