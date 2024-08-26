3a70c815c5a04e4e506400ae1d92af61db988a00
nebi <nebi@nebi.com>
2024-08-26 16:49:57 +0100

    Remove final from iscomplete definition

--- to_do_list/lib/models/task.dart
+++ to_do_list/lib/models/task.dart
@@ -1,7 +1,7 @@
 class Task {
   final String title;
   final String description;
-  final bool isComplete;
+  bool isComplete;
 
   Task({required this.title, required this.description, this.isComplete = false});
 }
