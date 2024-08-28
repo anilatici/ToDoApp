a41ab6c9dc28802e2f771bd140d3b4c8e515b01e
nebi <nebi@nebi.com>
2024-08-28 11:59:17 +0100

    Update main.dart imports

--- to_do_list/lib/main.dart
+++ to_do_list/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_list/pages/home.dart';
+import 'package:to_do_list/pages/incomplete_tasks.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
@@ -13,7 +14,7 @@ void main() async {
 class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'To Do App',
       home: HomePage(),
